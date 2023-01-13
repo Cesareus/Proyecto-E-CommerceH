365a4a9cf80e926a4d417a3e96fa95933e79cf78
kevinmakbel <kevinmakbel75@gmail.com>
2023-01-13 10:28:18 -0300

    corrigiendo footer

--- assets/js/indexHeader.js
+++ assets/js/indexHeader.js
@@ -3,18 +3,54 @@
 const down = document.querySelector('.header__category-down');
 const list = document.querySelector('.header__category-list');
 
+// OPCIÓN REDUNDADNTE
+
+// down.addEventListener('mouseover', function () {
+// 	list.style.visibility = 'visible';
+// });
+
+// down.addEventListener('mouseover', function () {
+// 	clearTimeout(timeout);
+// });
+
+// down.addEventListener('mouseout', function () {
+// 	timeout = setTimeout(function () {
+// 		list.style.visibility = 'hidden';
+// 	}, 200);
+// });
+
+// list.addEventListener('mouseover', function () {
+// 	clearTimeout(timeout);
+// });
+
+// list.addEventListener('mouseout', function () {
+// 	timeout = setTimeout(function () {
+// 		list.style.visibility = 'hidden';
+// 	}, 600);
+// });
+
+let timeout;
+
+// OPCIÓN DE CHATGPT :P
+
 down.addEventListener('mouseover', function () {
-	list.style.display = 'flex';
+	clearTimeout(timeout);
+	list.style.visibility = 'visible';
+});
+
+down.addEventListener('mouseout', function () {
+	timeout = setTimeout(() => {
+		list.style.visibility = 'hidden';
+	}, 200);
 });
 
 list.addEventListener('mouseover', function () {
 	clearTimeout(timeout);
+	list.style.visibility = 'visible';
 });
 
 list.addEventListener('mouseout', function () {
-	timeout = setTimeout(function () {
-		list.style.display = 'none';
-	}, 800);
+	timeout = setTimeout(() => {
+		list.style.visibility = 'hidden';
+	}, 600);
 });
-
-let timeout;
