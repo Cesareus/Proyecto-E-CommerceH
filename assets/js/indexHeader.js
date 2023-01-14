fa40bf1a7fb857f2f053c48cf1d5e5a1f3cb1a8f
StudioMontt <electri.mv@gmail.com>
2023-01-14 00:35:02 -0300

    Cambio en tiempos y correcion del menu desplegable

--- assets/js/indexHeader.js
+++ assets/js/indexHeader.js
@@ -3,18 +3,56 @@
 const down = document.querySelector('.header__category-down');
 const list = document.querySelector('.header__category-list');
 
+// OPCIÓN REDUNDADNTE
+
+// down.addEventListener('mouseover', function () {
+// 	list.style.visibility = 'visible';
+// });
+
+// down.addEventListener('mouseover', function () {
+// 	clearTimeout(timeout);
+// });
+
+// down.addEventListener('mouseout', function () {
+// 	timeout = setTimeout(function () {
+// 		list.style.visibility = 'hidden';
+// 	}, 800);
+// });
+
+// list.addEventListener('mouseover', function () {
+// 	clearTimeout(timeout);
+// });
+
+// list.addEventListener('mouseout', function () {
+// 	timeout = setTimeout(function () {
+// 		list.style.visibility = 'hidden';
+// 	}, 800);
+// });
+
+
+let timeout;
+
+// OPCIÓN DE CHATGPT :P
+
 down.addEventListener('mouseover', function () {
+	clearTimeout(timeout);
 	list.style.visibility = 'visible';
 });
 
+down.addEventListener('mouseout', function () {
+    timeout = setTimeout(() => {
+        list.style.visibility = 'hidden';
+    }, 200);
+});
+
 list.addEventListener('mouseover', function () {
-	clearTimeout(timeout);
+    clearTimeout(timeout);
+    list.style.visibility = 'visible';
 });
 
 list.addEventListener('mouseout', function () {
-	timeout = setTimeout(function () {
-		list.style.visibility = 'hidden';
-	}, 800);
+    timeout = setTimeout(() => {
+        list.style.visibility = 'hidden';
+    }, 600);
 });
 
-let timeout;
