cea41b60aad045bcdce248058e619ae8f1cfea17
kevinmakbel <kevinmakbel75@gmail.com>
2023-01-25 17:03:40 -0300

    modificando ajax de index

--- assets/mvc/View/Index/indexLeerProductos.js
+++ assets/mvc/View/Index/indexLeerProductos.js
@@ -6,7 +6,7 @@ const listCategory = document.querySelector(".header__category-list")
 
 const getProducts= async () =>{
   try {
-    await productosServices.ajax('./assets/mvc/controller/productos.php?op=listar', '').done(function (info) {
+    await productosServices.ajax('https://talyx.com.ar/assets/mvc/controller/productos.php?op=listar', '').done(function (info) {
       listProducts = JSON.parse(info);
     })}catch(e){
       console.log(e);
