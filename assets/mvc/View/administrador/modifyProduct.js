c00cb619317a113bae395afea3e44b2b8c711617
kevinmakbel <kevinmakbel75@gmail.com>
2023-03-01 19:33:21 -0300

    dando valores por defecto a modificar producto

--- assets/mvc/View/administrador/modifyProduct.js
+++ assets/mvc/View/administrador/modifyProduct.js
@@ -3,12 +3,16 @@ import { services, URL } from '../servicesJS/services.js'
 
 export const modifyProductsForm = document.getElementById('modalModifyProducts__form')
 
-export function modifyProduct (productId) {
-  modifyProductsForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    handleSubmit(productId)
-  })
-  modifyProductsForm.setAttribute('submit-listener', true)
+export async function modifyProduct (productId) {
+  try {
+    modifyProductsForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      handleSubmit(productId)
+    })
+    modifyProductsForm.setAttribute('submit-listener', true)
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 export function handleSubmit (productId) {
@@ -16,13 +20,21 @@ export function handleSubmit (productId) {
   data.append('ID', productId)
   try {
     services.ajax(`${URL}/assets/mvc/controller/productos.php?op=Modify`, data).done((info) => {
-      console.log(info)
       if (info === true) {
         window.location.reload()
       } else {
         console.log(info)
       }
     })
+  } catch (e) {
+    console.log(e)
+  }
+}
+export async function getProductForId (ID) {
+  try {
+    const data = new FormData()
+    data.append('ID', ID)
+    return await services.ajax(`${URL}/assets/mvc/controller/productos.php?op=getproductid`, data).done(info => JSON.parse(info))
   } catch (e) {
     console.log(e)
   }
